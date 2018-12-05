fc0ad39f74841fc1bb6867d41ca4f464af0e103c
John Whittington <git@jbrengineering.co.uk>
2018-12-05 08:33:38 +0000

    WS2812-core added with basic framework in top

--- top.v
+++ top.v
@@ -1,12 +1,18 @@
 `include "modulo.v"
+`include "ws2812-core/ws2812.v"
 
-module top(hwclk, led1, led2, led3, led4);
+module top(hwclk, led1, led2, led3, led4, ws_data);
   /* I/O */
   input hwclk;
   output led1;
   output led2;
   output led3;
   output led4;
+  output ws_data;
+
+  reg reset = 1;
+  always @(posedge hwclk)
+      reset <= 0;
 
   /* 1 Hz clock generation (from 12 MHz) */
   reg clk_1 = 1'b0;
@@ -60,7 +66,10 @@ module top(hwclk, led1, led2, led3, led4);
     end
   end
 
-  /* always @ (posedge clk_1) begin */
-  /* end */
+  reg [23:0] led_rgb_data = 24'h00_00_10;
+  reg [7:0] led_num = 0;
+  wire led_write = clk_1;
+
+  ws2812 #(.NUM_LEDS(16)) ws2812_inst(.data(ws_data), .clk(hwclk), .reset(reset), .rgb_data(led_rgb_data), .led_num(led_num), .write(led_write));
 
 endmodule
